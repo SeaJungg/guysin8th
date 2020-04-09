bd606353b56b410fad4d2b93a1ffd759a7fc5eb6
seulong <kjkj3468@naver.com>
2020-04-10 02:08:45 +0900

    개별페이지 및 STATIC파일 추가

--- main/views.py
+++ main/views.py
@@ -3,47 +3,50 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def show(request):
+    return render(request, 'show.html')
+
 def CJH(request):
-    return render(request, '.CJH.html')
+    return render(request, 'showyourpage/CJH.html')
 
 def JJW(request):
-    return render(request, 'JJW.html')
+    return render(request, 'showyourpage/JJW.html')
 
 def KCW(request):
-    return render(request, 'KCW.html')
+    return render(request, 'showyourpage/KCW.html')
 
 def KHC(request):
-    return render(request, 'KHC.html')
+    return render(request, 'showyourpage/KHC.html')
 
 def KJH(request):
-    return render(request, 'KJH.html')
+    return render(request, 'showyourpage/KJH.html')
 
 def KJE(request):
-    return render(request, 'KJE.html')
+    return render(request, 'showyourpage/KJE.html')
 
 def KJW(request):
-    return render(request, 'KJW.html')
+    return render(request, 'showyourpage/KJW.html')
 
 def KMJ(request):
-    return render(request, 'KMJ.html')
+    return render(request, 'showyourpage/KMJ.html')
 
 def LEA(request):
-    return render(request, 'LEA.html')
+    return render(request, 'showyourpage/LEA.html')
 
 def LMH(request):
-    return render(request, 'LMH.html')
+    return render(request, 'showyourpage/LMH.html')
 
 def LMJ(request):
-    return render(request, 'LMJ.html')
+    return render(request, 'showyourpage/LMJ.html')
 
 def LYB(request):
-    return render(request, 'LYB.html')
+    return render(request, 'showyourpage/LYB.html')
 
 def PJY(request):
-    return render(request, 'PJY.html')
+    return render(request, 'showyourpage/PJY.html')
 
 def SBD(request):
-    return render(request, 'SBD.html')
+    return render(request, 'showyourpage/SBD.html')
 
 def WIA(request):
-    return render(request, 'WIA.html')
+    return render(request, 'showyourpage/WIA.html')
