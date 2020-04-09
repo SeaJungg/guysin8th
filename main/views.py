8c0b0e8d5681c3f7b9b7384ae4043a15c3bead24
SeaJungg <wantedsaea@gmail.com>
2020-04-09 23:16:18 +0900

    views에 연결쓰

--- main/views.py
+++ main/views.py
@@ -2,3 +2,48 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def CJH(request):
+    return render(request, '.CJH.html')
+
+def JJW(request):
+    return render(request, 'JJW.html')
+
+def KCW(request):
+    return render(request, 'KCW.html')
+
+def KHC(request):
+    return render(request, 'KHC.html')
+
+def KJH(request):
+    return render(request, 'KJH.html')
+
+def KJE(request):
+    return render(request, 'KJE.html')
+
+def KJW(request):
+    return render(request, 'KJW.html')
+
+def KMJ(request):
+    return render(request, 'KMJ.html')
+
+def LEA(request):
+    return render(request, 'LEA.html')
+
+def LMH(request):
+    return render(request, 'LMH.html')
+
+def LMJ(request):
+    return render(request, 'LMJ.html')
+
+def LYB(request):
+    return render(request, 'LYB.html')
+
+def PJY(request):
+    return render(request, 'PJY.html')
+
+def SBD(request):
+    return render(request, 'SBD.html')
+
+def WIA(request):
+    return render(request, 'WIA.html')
