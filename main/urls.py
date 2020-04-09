6ad1c3520ca4ab91a72c045d24407ead4bad245e
seulong <kjkj3468@naver.com>
2020-04-09 23:50:44 +0900

    urls.py

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,19 @@ from. import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('CJH/', views.CJH, name="CJH" ),
+    path('JJW/', views.CJH, name="JJW" ),
+    path('KCW/', views.KCW, name="KCW" ),
+    path('KHC/', views.KHC, name="KHC" ),
+    path('KJH/', views.KJH, name="KJH" ),
+    path('KJE/', views.KJW, name="KJW" ),
+    path('KMJ/', views.KMJ, name="KMJ" ),
+    path('LEA/', views.LEA, name="LEA" ),
+    path('LMH/', views.LMH, name="LMH" ),
+    path('LYB/', views.LYB, name="LYB" ),
+    path('PJY/', views.PJY, name="PJY" ),
+    path('SBD/', views.SBD, name="SBD" ),
+    path('WIA/', views.WIA, name="WIA" ),
+    path('KJW/', views.KJW, name="KJW" ),
+    path('LMJ/', views.KJW, name="LMJ" ),
 ]
